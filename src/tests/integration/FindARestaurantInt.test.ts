f341d8bbdef564c953b0a4b9e7388021f697253f
ocsoares <casoares016@gmail.com>
2023-01-05 22:27:10 -0300

    test: integration test to find a restaurant

--- src/tests/integration/FindARestaurantInt.test.ts
+++ src/tests/integration/FindARestaurantInt.test.ts
@@ -1,10 +1,35 @@
+import request from 'supertest';
+import { BadRequestErrorMessages } from '../../@types/errorAPIMessages';
+import { app } from '../../app';
+import { getTokenWithTestUser } from '../../utils/testUtilts';
+
 describe('Find a restaurant Integration Test', () => {
+    const findARestaurantURLRoute = `/api/restaurant/COLOCAR O CERTO QUANDO FOR FAZER`;
+    const findARestaurantURLRouteWrongParameter = '/api/restaurant/invalid_restaurant';
+    const TEST_TOKEN = getTokenWithTestUser();
 
     // it('Should be possible to find a restaurant', async () => {
 
     // });
 
     it('Should be NOT find a restaurant', async () => {
+        const getResponse = await findARestaurantGetRoute(
+            findARestaurantURLRouteWrongParameter,
+            TEST_TOKEN
+        );
+        const expectBodyMessage: BadRequestErrorMessages = 'Restaurante não encontrado !';
 
+        expect(getResponse.body.message).toBe(expectBodyMessage);
+        expect(getResponse.statusCode).toBe(400);
     });
 });
+
+async function findARestaurantGetRoute(
+    urlRoute: string,
+    token: string
+): Promise<request.Response> {
+    const getResponse = await request(app).get(urlRoute)
+        .set('Authorization', `Bearer ${token}`);
+
+    return getResponse;
+}
