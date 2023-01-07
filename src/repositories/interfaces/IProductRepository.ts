dfedcde8529f3d9a5b1328c83b68c258c28a0ced
ocsoares <casoares016@gmail.com>
2023-01-07 20:30:04 -0300

    refactor: improving the create a product json return

--- src/repositories/interfaces/IProductRepository.ts
+++ src/repositories/interfaces/IProductRepository.ts
@@ -3,5 +3,6 @@ import { IProduct } from "../../models/IProduct";
 export interface IProductRepository {
     findProductById(id: string): Promise<IProduct>;
     findProductByName(name: string): Promise<IProduct>;
+    findAllProducts(): Promise<Array<IProduct>>;
     createProduct(data: IProduct): Promise<IProduct>;
 }
--- src/useCases/productUseCases/createProduct/CreateProductUseCase.ts
+++ src/useCases/productUseCases/createProduct/CreateProductUseCase.ts
@@ -2,6 +2,7 @@ import { IUseCase } from "../../../@types/interfaces/IUseCase";
 import { BadRequestAPIError, InternalServerErrorAPI } from "../../../helpers/ErrorAPIHelper";
 import { IProduct } from "../../../models/IProduct";
 import { IProductRepository } from "../../../repositories/interfaces/IProductRepository";
+import { ICreateProductRequest } from "./ICreateProduct";
 
 export class CreateProductUseCase implements IUseCase {
     constructor(
@@ -22,6 +23,16 @@ export class CreateProductUseCase implements IUseCase {
             throw new InternalServerErrorAPI('Não foi possível registrar o produto. Tente novamente mais tarde.');
         }
 
-        return newProduct;
+        const mainInformationOfAllProducts: ICreateProductRequest = {
+            photo_url: newProduct.photo_url,
+            name: newProduct.name,
+            price: newProduct.price,
+            category: newProduct.category,
+            description_onsale: newProduct.description_onsale,
+            new_price_onsale: newProduct.new_price_onsale,
+            day_and_hour_onsale: newProduct.day_and_hour_onsale
+        };
+
+        return mainInformationOfAllProducts;
     }
 }
