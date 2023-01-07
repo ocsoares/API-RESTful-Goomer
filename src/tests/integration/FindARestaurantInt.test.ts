90b30d4ed26bf235ca726e41914d20bc78e2f475
ocsoares <casoares016@gmail.com>
2023-01-06 21:46:23 -0300

    test: add authorization test to find a restaurant integration test

--- src/tests/integration/FindARestaurantInt.test.ts
+++ src/tests/integration/FindARestaurantInt.test.ts
@@ -1,26 +1,36 @@
 import request from 'supertest';
-import { BadRequestErrorMessages } from '../../@types/errorAPIMessages';
+import { BadRequestErrorMessages, UnauthorizedErrorMessages } from '../../@types/errorAPIMessages';
 import { app } from '../../app';
 import { getTokenWithTestUser } from '../../utils/testUtilts';
 
 describe('Find a restaurant Integration Test', () => {
-    const findARestaurantURLRoute = `/api/restaurant/COLOCAR O CERTO QUANDO FOR FAZER`;
-    const findARestaurantURLRouteWrongParameter = '/api/restaurant/invalid_restaurant';
+    const findARestaurantURLRoute = `/api/restaurant/`;
     const TEST_TOKEN = getTokenWithTestUser();
 
     // it('Should be possible to find a restaurant', async () => {
 
     // });
 
-    it('Should be NOT find a restaurant', async () => {
+    it('Should be NOT find a restaurant if id is invalid', async () => {
         const getResponse = await findARestaurantGetRoute(
-            findARestaurantURLRouteWrongParameter,
+            findARestaurantURLRoute + 'invalid_id',
             TEST_TOKEN
         );
         const expectBodyMessage: BadRequestErrorMessages = 'Restaurante não encontrado !';
 
         expect(getResponse.body.message).toBe(expectBodyMessage);
         expect(getResponse.statusCode).toBe(400);
+    });
+
+    it('Should NOT be possible to find a restaurant if auth token is invalid', async () => {
+        const getResponse = await findARestaurantGetRoute(
+            findARestaurantURLRoute + 'any_id',
+            'invalid_token'
+        );
+        const expectBodyMessage: UnauthorizedErrorMessages = 'Token inválido ou expirado !';
+
+        expect(getResponse.body.message).toBe(expectBodyMessage);
+        expect(getResponse.statusCode).toBe(401);
     });
 });
 
