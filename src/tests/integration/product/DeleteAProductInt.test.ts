99335fa48298ced3e4f141b9811dbeec13f4ce2f
ocsoares <casoares016@gmail.com>
2023-01-11 19:37:15 -0300

    test: add mock integration test to delete a product

--- src/tests/integration/product/DeleteAProductInt.test.ts
+++ src/tests/integration/product/DeleteAProductInt.test.ts
@@ -1,22 +1,33 @@
 import request from 'supertest';
+import { Request, Response } from 'express';
 import { UnauthorizedErrorMessages } from '../../../@types/errorAPIMessages';
 import { app } from '../../../app';
-import { getTokenWithTestUser } from '../../../utils/testUtilts';
+import { deleteAProductController } from '../../../factories/useCases/productUseCases/deleteAProductRepository';
+import { getTokenWithTestUser, testBodyReturnFunction } from '../../../utils/testUtilts';
 
 describe('Delete a product Integration Test', () => {
     const urlRoute = '/api/product/';
     const TEST_TOKEN = getTokenWithTestUser();
+    const testBodyReturn = testBodyReturnFunction('Produto deletado com sucesso !');
 
-    // it('Should be possible to delete a product', async () => {
-    //     const mReq = ({
-    //         params: { id: 'any_id_params' },
-    //         body: {
-    //             ...testBodyReturn
-    //         }
-    //     } as unknown) as Request;
+    it('Should be possible to delete a product', async () => {
+        const mReq = ({
+            params: { id: 'any_id_params' },
+            body: {
+                ...testBodyReturn
+            }
+        } as unknown) as Request;
 
-    //     const mRes = ({ status: jest.fn().mockReturnThis(), json: jest.fn() } as unknown) as Response;
-    // });
+        const mRes = ({ status: jest.fn().mockReturnThis(), json: jest.fn() } as unknown) as Response;
+
+        try {
+            await deleteAProductController.handle(mReq, mRes);
+        }
+        catch (error: any) {
+            expect(mReq.body.message).toBe('Produto deletado com sucesso !');
+            expect(mReq.body.product).toHaveProperty('id');
+        }
+    });
 
     it('Should NOT be possible to delete a product if product id is invalid', async () => {
         const getResponse = await deleteAProductDeleteRoute(
